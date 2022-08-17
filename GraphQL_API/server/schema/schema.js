a8b8b79d403c09ce00a8be69a57f07f9538c215c
Tres Serio <treserio@hotmail.com>
2022-08-17 00:32:26 -0500

    3 init

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -3,17 +3,28 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLID,
 } = require('graphql');
 const lod = require('lodash');
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
-      id: {type: GraphQLString},
+      id: {type: GraphQLID},
       title: {type: GraphQLString},
       weight: {type: GraphQLInt},
       description: {type: GraphQLString},
     }
+});
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: {
+    id: {type: GraphQLID},
+    title: {type: GraphQLString},
+    weight: {type: GraphQLInt},
+    description: {type: GraphQLString},
+  }
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -22,9 +33,16 @@ const RootQuery = new GraphQLObjectType({
     task: {
       type: TaskType,
       args: {
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
       },
       resolve: (parent, args) => lod.find(tasks, {id: args.id}),
+    },
+    project: {
+      type: ProjectType,
+      args: {
+        id: {type: GraphQLID},
+      },
+      resolve: (parent, args) => lod.find(projects, {id: args.id}),
     }
   },
 });
@@ -44,6 +62,23 @@ const tasks = [
     description:
       "Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order",
   }
+];
+
+const projects = [
+  {
+    id: "1",
+    title: "Advanced HTML",
+    weight: 1,
+    description:
+      "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
+  },
+  {
+    id: "2",
+    title: "Bootstrap",
+    weight: 1,
+    description:
+      "Bootstrap is a free and open-source front-end web framework for developing with HTML, CSS, and JS. It contains HTML, CSS, and JS components for faster and easier web development. Bootstrap is the world’s most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
+  },
 ];
 
 const schema = new GraphQLSchema({
