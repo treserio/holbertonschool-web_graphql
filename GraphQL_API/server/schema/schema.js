172af5f320154631faca8478ebc0bf2887e24f14
Tres Serio <treserio@hotmail.com>
2022-08-17 00:22:28 -0500

    Task 2 finished

---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,53 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} = require('graphql');
+const lod = require('lodash');
+
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: {
+      id: {type: GraphQLString},
+      title: {type: GraphQLString},
+      weight: {type: GraphQLInt},
+      description: {type: GraphQLString},
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: {
+        id: {type: GraphQLString},
+      },
+      resolve: (parent, args) => lod.find(tasks, {id: args.id}),
+    }
+  },
+});
+
+const tasks = [
+  {
+    id: "1",
+    title: "Create your first webpage",
+    weight: 1,
+    description:
+      "Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)",
+  },
+  {
+    id: "2",
+    title: "Structure your webpage",
+    weight: 1,
+    description:
+      "Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order",
+  }
+];
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+module.exports = schema;
