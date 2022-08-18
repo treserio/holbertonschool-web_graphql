d2dc9530d8562eefd347abfb8e9e20de65f16472
Tres Serio <treserio@hotmail.com>
2022-08-17 20:12:57 -0500

    8 init

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -12,7 +12,7 @@ const TaskType = new gql.GraphQLObjectType({
       description: {type: gql.GraphQLString},
       project: {
         type: ProjectType,
-        resolve: (parent) => lod.find(projects, {id: parent.projectId}),
+        resolve: (parent) => Project.findById(parent.projectId),
       },
     })
 });
@@ -26,7 +26,7 @@ const ProjectType = new gql.GraphQLObjectType({
     description: {type: gql.GraphQLString},
     tasks: {
       type: new gql.GraphQLList(TaskType),
-      resolve: (parent) => lod.filter(tasks, {projectId: parent.id}),
+      resolve: (parent) => Task.find({projectId: parent.id}),
     },
   }
 });
@@ -39,61 +39,25 @@ const RootQuery = new gql.GraphQLObjectType({
       args: {
         id: {type: gql.GraphQLID},
       },
-      resolve: (_, args) => lod.find(tasks, {id: args.id}),
+      resolve: (_, args) => Task.findById(args.id),
     },
     tasks: {
       type: new gql.GraphQLList(TaskType),
-      resolve: () => tasks,
+      resolve: () => Task.find(),
     },
     project: {
       type: ProjectType,
       args: {
         id: {type: gql.GraphQLID},
       },
-      resolve: (_, args) => lod.find(projects, {id: args.id}),
+      resolve: (_, args) => Project.findById(args.id),
     },
     projects: {
       type: new gql.GraphQLList(ProjectType),
-      resolve: () => projects,
+      resolve: () => Project.find(),
     },
   },
 });
-
-const tasks = [
-  {
-    id: "1",
-    title: "Create your first webpage",
-    weight: 1,
-    description:
-      "Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)",
-    projectId: "62fd7a9c026aae0d85d3a20f",
-  },
-  {
-    id: "2",
-    title: "Structure your webpage",
-    weight: 1,
-    description:
-      "Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order",
-    projectId: "62fd7a9c026aae0d85d3a20f",
-  }
-];
-
-const projects = [
-  {
-    id: "1",
-    title: "Advanced HTML",
-    weight: 1,
-    description:
-      "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
-  },
-  {
-    id: "2",
-    title: "Bootstrap",
-    weight: 1,
-    description:
-      "Bootstrap is a free and open-source front-end web framework for developing with HTML, CSS, and JS. It contains HTML, CSS, and JS components for faster and easier web development. Bootstrap is the world’s most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
-  },
-];
 
 const mutation = new gql.GraphQLObjectType({
   name: 'Mutation',
@@ -115,8 +79,9 @@ const mutation = new gql.GraphQLObjectType({
     },
     addTaskz: {
       type: new gql.GraphQLList(TaskType),
-      resolve: () => {
-        for (t of RootQuery._fields.tasks.resolve()) {
+      resolve: async () => {
+        res = await RootQuery._fields.tasks.resolve();
+        for (t of res) {
           if (t.title && t.weight && t.description) {
             new Task({
               title: t.title,
@@ -126,7 +91,7 @@ const mutation = new gql.GraphQLObjectType({
             }).save();
           }
         }
-        return tasks;
+        return res;
       }
     },
     addProject: {
@@ -144,8 +109,9 @@ const mutation = new gql.GraphQLObjectType({
     },
     addProjectz: {
       type: new gql.GraphQLList(ProjectType),
-      resolve: () => {
-        for (p of RootQuery._fields.projects.resolve()) {
+      resolve: async () => {
+        res = await RootQuery._fields.projects.resolve();
+        for (p of res) {
           if (p.title && p.weight && p.description) {
             new Project({
               title: p.title,
@@ -154,7 +120,7 @@ const mutation = new gql.GraphQLObjectType({
             }).save();
           }
         }
-        return projects;
+        return res;
       }
     },
   }
